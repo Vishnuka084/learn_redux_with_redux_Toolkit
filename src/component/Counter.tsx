a0a3b23bdb613fc5c4532fba1c4394227d681fb8
vishnuk084 <vishnukayahan84@gmail.com>
2024-08-27 15:46:59 +0530

    Updated Contnet

--- src/component/Counter.tsx
+++ src/component/Counter.tsx
@@ -1,6 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../state/store";
-import { decrement, increment } from "../state/counter/counterSlice";
+import {
+  decrement,
+  increment,
+  incrementByAmount
+} from "../state/counter/counterSlice";
 
 const Counter = () => {
   const count = useSelector((state: RootState) => state.counter.value);
@@ -12,7 +16,9 @@ const Counter = () => {
         {count}
       </h2>
       <div>
-        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(incrementByAmount(10))}>
+          increment
+        </button>
         <button onClick={() => dispatch(decrement())}>decrement</button>
       </div>
     </div>
